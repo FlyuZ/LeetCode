ba63318e2930702f171ca042ea254d676527c6fd
FlyuZ <522893161@qq.com>
2018-03-24 16:31:00 +0800

    3.24

---
+++ 236LowestCommonAncestorofaBinaryTree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+    return dfs(root, p, q);
+  }
+  TreeNode *dfs(TreeNode *root, TreeNode *p, TreeNode *q) {
+    if (root == p || root == q || root == NULL)
+      return root;
+    TreeNode *parent1 = dfsTraverse(root->left, p, q);
+    TreeNode *parent2 = dfsTraverse(root->right, p, q);
+    if (parent1 && parent2)
+      return root;
+    else
+      return parent1 ? parent1 : parent2;
+  }
+};
