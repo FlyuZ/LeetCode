79a97e56e198ce6d18d2678de46deebb4084ed04
FlyuZ <522893161@qq.com>
2020-05-01 22:57:59 +0800

    20200501

---
+++ merge-two-sorted-lists.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        } else {
+            cur.next = l2;
+        }
+        return dummyHead.next;
+    }
+}
